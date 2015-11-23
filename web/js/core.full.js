79525724275e105073aa4e2bbde187b430cb4cea
mhtmhn <mht.mhn@gmail.com>
2015-11-23 19:21:35 +0530

    Firefox Compatibility, Remove Address

--- web/js/core.full.js
+++ web/js/core.full.js
@@ -18,13 +18,86 @@ $(window).load(function () {
 */
 //]]>
 
-//Smooth scroll
+//Firefox Friendly Smooth Scroll
+$(function(){
+
+var $links = $("a");
+var $sections = $(".content-section");
+
+var headerOffset = 65;
+var menuClickMode = false;
+var currentSection;			//current section ID, no hashtag 
+//smooth scroll on click
+$links.on("click", function(event){
+event.preventDefault();
+menuClickMode = true;
+var nextSection = $(this).attr("href");
+selectMenu(nextSection);
+goToSection(nextSection);
+});
+
+//selects menu, etc
+$(document).on("scroll", checkSections);
+$(window).on("resize", checkSections);
+
+//trigger scroll to select first menu
+$(document).trigger("scroll");
+/*
+Logic
+*/
+
+//smooth scroll animation
+function goToSection(_section){
+TweenMax.to( window, 1, {scrollTo: {y: $(_section).offset().top - headerOffset, autoKill:false}, ease:Power1.easeInOut, onStart:disableScroll, onComplete:enableScroll} );
+}	
+
+//selects menu by it's href
+function selectMenu(_menuByHref){
+if(_menuByHref.charAt(0) == "#"){
+_menuByHref = _menuByHref.substr(1);
+} 				
+$links.filter("[href='#" +  _menuByHref + "']").addClass("nav-active").siblings().removeClass("nav-active");	
+}
+
+/*
+On scroll
+*/
+
+function checkSections(){
+var scrollTop = $(window).scrollTop();
+
+$sections.each(function(){
+
+if($(this).offset().top + $(this).height() - headerOffset > scrollTop){
+
+if(currentSection != $(this).attr("id") ){
+currentSection = $(this).attr("id");
+//console.log("new current section with id: " + currentSection);
+
+//don't select passing by menus if button is clicked
+if(!menuClickMode){
+selectMenu($(this).attr("id"));
+}
+}
+
+return false;
+
+}//end if 
+
+});//end each
+}//end checkSections
+
+});
+//End of Firefox Friendly Smooth Scroll
+
+
+//Smooth scroll mouse
 $(function() {
   var $window = $(window);
-  var scrollTime = 0.15;
+  var scrollTime = 0.45;
   var scrollDistance = 150;
-  var deltaDivide = 200;
-  var detailDivide= 5;
+  var deltaDivide = 100;
+  var detailDivide= 2.5;
   $window.on("mousewheel DOMMouseScroll", function(event) {
     event.preventDefault();
     var delta = event.originalEvent.wheelDelta / deltaDivide || -event.originalEvent.detail / detailDivide;
@@ -37,6 +110,8 @@ $(function() {
       ease: Sine.easeOut, y: 0, autokill:true});
   });
 });
+/*
+//Smooth scroll obsolete
 $(function() {
   $('a[href*=#]:not([href=#])').click(function(e) {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -50,7 +125,7 @@ $(function() {
     }
   });
 });
-
+*/
 //Disable Mouse during tween, continue onComplete
 // left: 37, up: 38, right: 39, down: 40,
 // spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
